4f442db4b90e9ab13a06c31c50e44e08eca124bd
ablamhamdi <ablamhamdi@sqli.com>
2023-03-28 09:38:42 +0000

    commitOne

---
+++ AppMeteo/Client/Pages/FetchData.razor.cs
@@ -0,0 +1,19 @@
+﻿using AppMeteo.Shared;
+using AppMeteo.Shared.Data;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace AppMeteo.Client.Pages
+{
+    public partial class FetchData
+    {
+        [Inject]
+        public WeatherForecastService weather { get; set; }
+        private WeatherForecast[]? forecasts;
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await weather.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
---
+++ AppMeteo/Shared/Data/WeatherForecastService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AppMeteo.Shared.Data
+{
+    public class WeatherForecastService
+    {
+        private static readonly string[] summaries = new[]
+                 {
+                //"Freezing", "Bracing", "Chilly", "Cool", "Mild", 
+                //"Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+                "Cloudy", "Rainy", "Sunny"
+                };
+        public async Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+        {
+            var rng = new Random();
+           // var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+            return await Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = summaries[rng.Next(summaries.Length)]
+            }).ToArray());
+        }
+
+    }
+}
